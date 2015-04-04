e1eb82aec937afa479a1f031c339e9c42b2c3e93
Julien Guanzon <julienguanzon@gmail.com>
2015-04-03 23:34:19 -0600

    commit

---
+++ ios-competency/ios-competencyTests/ios_competencyTests.m
@@ -0,0 +1,40 @@
+//
+//  ios_competencyTests.m
+//  ios-competencyTests
+//
+//  Created by Julien Guanzon on 3/26/15.
+//  Copyright (c) 2015 Julien Guanzon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ios_competencyTests : XCTestCase
+
+@end
+
+@implementation ios_competencyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
